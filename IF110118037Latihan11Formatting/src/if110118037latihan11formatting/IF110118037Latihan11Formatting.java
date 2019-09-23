9c05692d8be7e4a7fdde19c8f0de47eb253d6619
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-09-23 14:57:31 +0700

    Add files via upload

---
+++ IF110118037Latihan11Formatting/src/if110118037latihan11formatting/IF110118037Latihan11Formatting.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if110118037latihan11formatting;
+
+/**
+ * @author LENOVO
+ * NAMA      : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM       : 10118037
+ * Deskripsi Program   : Program ini berisi program Formatting angka keluaran.
+ */
+public class IF110118037Latihan11Formatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n" , i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f :%f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+    
+}
